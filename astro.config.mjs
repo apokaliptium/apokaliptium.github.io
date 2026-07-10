b14a0f76ce08b45259de497a404d5ef03aa53d1e
Sett Sarverott <sett@sarverott.com>
2026-07-10 08:55:58 +0200

    adding handling of usage with obsidian to make easier updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,37 +1,44 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import starlight from '@astrojs/starlight';
 
-import sitemap from '@astrojs/sitemap';
-
-import partytown from '@astrojs/partytown';
-
-import markdoc from '@astrojs/markdoc';
-
-import mdx from '@astrojs/mdx';
+import markdoc from "@astrojs/markdoc";
+import mdx from "@astrojs/mdx";
+import partytown from "@astrojs/partytown";
+import sitemap from "@astrojs/sitemap";
+import starlight from "@astrojs/starlight";
+import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://apokaliptium.github.io/",
-    integrations: [starlight(
-        {
-            title: 'Apokaliptium',
-            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/apokaliptium' }],
-            sidebar: [
-                {
-                    label: 'Guides',
-                    items: [{ autogenerate: { directory: 'guides' } }],
-                },
-                {
-                    label: 'Concept',
-                    items: [{ autogenerate: { directory: 'concept' } }],
-                },
-                {
-                    label: 'Reference',
-                    items: [{ autogenerate: { directory: 'reference' } }],
-                },
-            ],
-            credits: true
-        }
-        ), sitemap(), partytown(), markdoc(), mdx()],
+	site: "https://apokaliptium.github.io/",
+	integrations: [
+		starlight({
+			title: "Apokaliptium",
+			social: [
+				{
+					icon: "github",
+					label: "GitHub",
+					href: "https://github.com/apokaliptium",
+				},
+			],
+			sidebar: [
+				{
+					label: "Guides",
+					items: [{ autogenerate: { directory: "guides" } }],
+				},
+				{
+					label: "Concept",
+					items: [{ autogenerate: { directory: "concept" } }],
+				},
+				{
+					label: "Reference",
+					items: [{ autogenerate: { directory: "reference" } }],
+				},
+			],
+			credits: true,
+		}),
+		sitemap(),
+		partytown(),
+		markdoc(),
+		mdx(),
+	],
 });
