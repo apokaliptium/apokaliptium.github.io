c941bd182048db7b1166401460bcee1553b3843e
Sett Sarverott <sett@sarverott.com>
2026-07-10 07:31:23 +0200

    adding partytown by astro integration wrapper

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,31 +4,30 @@ import starlight from '@astrojs/starlight';
 
 import sitemap from '@astrojs/sitemap';
 
+import partytown from '@astrojs/partytown';
+
 // https://astro.build/config
 export default defineConfig({
-	site: "https://apokaliptium.github.io/",
-    integrations: [
-		starlight(
-			{
-				title: 'Apokaliptium',
-				social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/apokaliptium' }],
-				sidebar: [
-					{
-						label: 'Guides',
-						items: [{ autogenerate: { directory: 'guides' } }],
-					},
-					{
-						label: 'Concept',
-						items: [{ autogenerate: { directory: 'concept' } }],
-					},
-					{
-						label: 'Reference',
-						items: [{ autogenerate: { directory: 'reference' } }],
-					},
-				],
-				credits: true
-			}
-		), 
-		sitemap()
-	],
+    site: "https://apokaliptium.github.io/",
+    integrations: [starlight(
+        {
+            title: 'Apokaliptium',
+            social: [{ icon: 'github', label: 'GitHub', href: 'https://github.com/apokaliptium' }],
+            sidebar: [
+                {
+                    label: 'Guides',
+                    items: [{ autogenerate: { directory: 'guides' } }],
+                },
+                {
+                    label: 'Concept',
+                    items: [{ autogenerate: { directory: 'concept' } }],
+                },
+                {
+                    label: 'Reference',
+                    items: [{ autogenerate: { directory: 'reference' } }],
+                },
+            ],
+            credits: true
+        }
+		), sitemap(), partytown()],
 });
